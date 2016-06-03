170a56b2a45f93b45e3aa9c3973313d1c19c46ec
Ben Incani <Ben Incani>
2016-06-03 13:18:03 +1000

    support wordpress in submodule + separate environment config

--- wp-config.php
+++ wp-config.php
@@ -27,24 +27,17 @@ define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
 /** see wp-content/themes */
 define('WP_DEFAULT_THEME', 'rhythm');
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
-
-/** MySQL database username */
-define('DB_USER', 'username_here');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+/**
+ * environment related settings e.g. dev OR prod
+ * used if you want to directly include an environment config (e.g. while in dev)
+ * otherwise this file is never changed only the wp-config-ENV.php files
+ * while wp-config-env.php is linked to the actual config via capistrano
+ */
+# define('WP_DEFAULT_ENV', 'dev');
+if ( defined('WP_DEFAULT_ENV') )
+	require_once(dirname(dirname(__FILE__)) . '/wp-config-' . WP_DEFAULT_ENV . '.php');
+else
+	require_once(dirname(dirname(__FILE__)) . '/wp-config-env.php');
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -55,14 +48,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'eFwgYHZldpg1aHXlYPwiQxfNhJFxsBr82SCRZ5gzTAWfOCHOnKsCqvYyNQ6fRemT');
+define('SECURE_AUTH_KEY',  'WT7P1aIk5l9A9pfLPnN5OpGxxQujYdGW0OrXTLbapoW0eDQf89YZa7onQlF0L6u8');
+define('LOGGED_IN_KEY',    'IEXm4XllRrmp9CdjwBsLFz4WqwYFX6I4Y6jDniOYyUxxGyL5u07NfVzKvbTgQNK0');
+define('NONCE_KEY',        'iMkPD1xfFQTep7BON5oV8FTolKUSNAuP1vpE7MLJJxlJVGtVIRWxVAnb3BMWri6N');
+define('AUTH_SALT',        'w1btMc4EdnwCGBXH2pWQbiWtsWCyA590uT0KzGG2yb72ndYrWVIJj6iC4dbZ1ym9');
+define('SECURE_AUTH_SALT', 'vf7hCFqHhz6ELdI2yg8CQB7jhVWJ5xQ66mx1nlGl25uKsaSkJE7HGgVlq1kwQZUB');
+define('LOGGED_IN_SALT',   'bChzSvJc6cGvG7spPEr8yKlQVPymaWlmwxNeWCBaGQny8NQbAbzEaUnx2Dzf6UOv');
+define('NONCE_SALT',       'OAJGA5CfC89o3wxqfXOpYjCDHUREOYDeRn1io0os4za1samHNZMyGAEZLI6dlDFM');
 
 /**#@-*/
 
@@ -74,19 +67,8 @@ define('NONCE_SALT',       'put your unique phrase here');
  */
 $table_prefix  = 'wp_';
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the Codex.
- *
- * @link https://codex.wordpress.org/Debugging_in_WordPress
- */
-define('WP_DEBUG', false);
+/** WordPress Localized Language, defaults to English. */
+define('WPLANG', '');
 
 /* That's all, stop editing! Happy blogging. */
 
